9dcede7ca3afc910965ad602de38c132ca6afc4b
Manuel Zippo <manuel.zippo01@universitadipavia.it>
2020-10-13 21:15:04 +0200

    Script can now be easily used for other image corruptions

--- image_corruptor.py
+++ image_corruptor.py
@@ -1,3 +1,8 @@
+"""
+Takes the image called 'img.png' in the current directory and applies some distortion to the 3 channels.
+The image corrupted is shown (together with the original one) and is saved as 'im2.png'.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,8 +14,7 @@ x = np.empty((256, 256, 3))
 x[:, :, 0] = r + 0.5
 x[:, :, 1] = g + 0.5
 x[:, :, 2] = b
-x = np.clip(x, 0, 1)
-x = plt.imread('im2.png')
+x = x.clip(0, 1)
 
 print(np.sum(im[:, :, 0] - x[:, :, 0]))
 print('\n')
@@ -23,7 +27,7 @@ plt.imshow(im)
 
 plt.figure(2)
 plt.imshow(x)
-plt.imsave('im2.png',x)
+plt.imsave('im2.png', x)
 plt.show()
 
 
